ec25076956004375d675fa16ba8ef564c894df32
oktoshi <devteam@okcash.co>
2024-11-02 06:26:55 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,23 +18,7 @@ jQuery(function($) {
 	});
 
 	//portfolio
-	$(window).load(function(){
-		$portfolio_selectors = $('.portfolio-filter >li>a');
-		if($portfolio_selectors!='undefined'){
-			$portfolio = $('.portfolio-items');
-			$portfolio.isotope({
-				itemSelector : 'li',
-				layoutMode : 'fitRows'
-			});
-			$portfolio_selectors.on('click', function(){
-				$portfolio_selectors.removeClass('active');
-				$(this).addClass('active');
-				var selector = $(this).attr('data-filter');
-				$portfolio.isotope({ filter: selector });
-				return false;
-			});
-		}
-	});
+
 
 	//contact form
 	var form = $('.contact-form');
